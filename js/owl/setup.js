0684cf79f37e41bd18470e97e712fbf09bd99abe
perkles <sarotec15@gmail.com>
2025-09-21 21:09:36 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,13 @@
+$ ( documento ). pronto ( função () { 
+ 
+  $ ( "#owl-demo" ). owlCarousel ({
+ 
+      autoPlay : 3000 , //Defina a reprodução automática para 3 segundos  
+ 
+      itens : 4 , 
+      itensDesktop : [ 1199 , 3 ], 
+      itensDesktopSmall : [ 979 , 3 ] 
+ 
+  });
+ 
+});
